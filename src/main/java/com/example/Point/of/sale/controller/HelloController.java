abde55a6e3f725f3a8d3d92c679b566a615daa8a
unknown <wwepwawet@gmail.com>
2025-12-13 23:42:53 +0100

    Test webhook déclenchement automatique

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -5,22 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
-public class HelloController {
-    @GetMapping
-    public String hello(){
-        return "Hello from spring boot application :)";
-    }
-
-    @GetMapping("/user")
-    public String getUsers(){
-        return "Users";
-    }
-
-
-    @GetMapping("/presentation")
-    public String getPresentation(){
-        return "presentation";
-    }
+@GetMapping
+public String hello() {
+    return "Hello from New Test :)";
 }
