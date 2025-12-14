c2ce01e3ba4807c44a512b63ece88d9e33d82071
unknown <wwepwawet@gmail.com>
2025-12-14 11:17:49 +0100

    Fix HelloController syntax

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.Point.of.sale.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@GetMapping
-public String hello() {
-    return "Hello from New Test :)";
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from New Test :)";
+    }
 }
